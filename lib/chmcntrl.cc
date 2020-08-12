b7cd344a7b6cf9832321dcba45a3217fade95e4b
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2020-08-12 13:03:47 +0900

    Enhanced logic for initial connection of server nodes

--- lib/chmcntrl.cc
+++ lib/chmcntrl.cc
@@ -520,6 +520,23 @@ bool ChmCntrl::EventLoop(void)
 						}
 					}
 				}
+			}else if(pEventSock){
+				// Server node
+				if(1L >= ImData.GetUpServerCount()){
+					// [NOTE]
+					// This case is that there are no other server node up.
+					if(pEventSock->InitialAllServerStatus()){
+						if(1L < ImData.GetUpServerCount()){
+							// Try to connect servers
+							//MSG_CHMPRN("Try to connect servers on RING(mode is SERVER).");
+							pEventSock->ConnectRing();		// not need to check result.
+						}else{
+							// sleep
+							struct timespec	sleeptime = {0, 100 * 1000 * 1000};		// 100ms
+							nanosleep(&sleeptime, NULL);
+						}
+					}
+				}
 			}
 			continue;
 		}
